05dd6466ec80367834c3f516d0d5197580ac986c
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-03-06 14:37:55 -0300

    insert Guzzle and Crawler in project

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-    echo "Iniciando com Composer.";
+
+require 'vendor/autoload.php';
+
+// Fazer uma busca via GET com Guzzler e imprimir na tela com Crawler via composer 
+use guzzlehttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new GuzzleHttp\Client();
+$resultado = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $resultado->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . '<br>';
+}
